a100975e8099a7994c4629ae4a950503d1a7e230
leandro0508 <86131955+leandro0508@users.noreply.github.com>
2021-06-18 22:41:02 -0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CadastroProd.Models;
+
+namespace CadastroProd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var produto = new Produto(); // aqui criamos o projeto
+            return View(produto); // Retornamos para a view dados
+        }
+        [HttpPost]
+        public ActionResult Index(Produto produto)
+        {
+            if (ModelState.IsValid) // Validamos o estado
+            {
+                return View("Resultado", produto);
+            }
+            return View(produto);
+        }
+        public ActionResult Resultado(Produto produto)
+        {
+            return View(produto);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web;
+
+
+namespace CadastroProd.Models
+{
+    public class Produto 
+    {
+        
+        [Display (Name = "Nº Código")]
+        public int Codigo { get; set; }
+        [Required(ErrorMessage = "O nome do produto é obrigatório!")]
+        public string NomeProd { get; set; }
+
+
+        [Required(ErrorMessage = "Insira a descrição do produto")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "Informe uma descrição de no minímo 10 caracteres a 200")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "O peso do produto é obrigatoriamente solicitado!")]
+        [Range(1.00, 18.99, ErrorMessage = "Peso inválido")]
+        public decimal Peso { get; set; }
+
+        [Required(ErrorMessage = "Informe a categoria do produto")]
+        public string Categoria { get; set; }
+
+        [Required(ErrorMessage = "O valor da unidade do produto é necessário!")]
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "O valor da unidade do produto está inválido!")]
+        public string ValorUnit { get; set; }
+    }
+}
